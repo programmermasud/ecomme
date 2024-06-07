cc5a5b3e222ef5d4c647c967e3c1def42454daa6
masudrana <masudrana.npakhimara@gmail.com>
2024-06-07 21:22:48 +0600

    added model file

--- management/models.py
+++ management/models.py
@@ -7,5 +7,12 @@ class contact(models.Model):
     age = models.IntegerField()
     phone = models.CharField(max_length=15)
     address = models.TextField()
+    is_active = models.BooleanField(default=False)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+
+    def __str__(self):
+
+        return f"{self.name} | {self.phone} | {self.address}"
 
 
