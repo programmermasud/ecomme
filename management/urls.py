11ba5954f603909fd86694ac7e3f36bc9e774d90
masudrana <masudrana.npakhimara@gmail.com>
2024-06-15 22:20:08 +0600

    add ignore file

--- management/urls.py
+++ management/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from .views import *
 
 urlpatterns = [
-    path('',home,name='home'),
+    path('',Home,name='Home'),
     path('contacts/', contacts, name='contacts'),
     path('about/',about,name='about'),
     path('contacts/details/<int:id>', details, name='details'),
